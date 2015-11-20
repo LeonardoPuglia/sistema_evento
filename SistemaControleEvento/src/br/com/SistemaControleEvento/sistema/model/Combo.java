95596b55ec4b9bb7ab9c20efe2b49d19fd1e391b
Leo <leo.puglia@hotmail>
2015-11-20 16:45:30 -0200

    Primeiro commit github

--- SistemaControleEvento/src/br/com/SistemaControleEvento/sistema/model/Combo.java
+++ SistemaControleEvento/src/br/com/SistemaControleEvento/sistema/model/Combo.java
@@ -1,8 +1,10 @@
 package br.com.SistemaControleEvento.sistema.model;
 
+import java.awt.Image;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,6 +19,14 @@ import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 import br.com.SistemaControleEvento.site.model.Imagem;
 
@@ -35,21 +45,37 @@ public class Combo {
 	private Long codCombo;
 	
 	@ManyToMany(fetch = FetchType.EAGER)
+	//@NotNull(message = "O campo produtos é obrigatório")
 	@JoinColumn(name = "id_produtoFK" , referencedColumnName = "id_produto", nullable = false)
 	private List<Produto> produtos;
 	
 	@OneToOne(fetch = FetchType.EAGER)
+	//@NotNull(message = "O campo salão é obrigatório")
 	@JoinColumn(name = "id_salaoFK" , referencedColumnName = "id_salao", nullable = false)
 	private SalaoFesta salaofesta;
+	
+	//@NotNull(message="O campo é obrigatório")
+	//@DecimalMin(value = "0.0",message = "Informe apenas valores positivos ou nulo para o campo preço")
+	//@DecimalMax(value = "999999.9",message = "Informe apenas valores menores que 999.999,99" )
 	@Column(name = "preco", precision = 7 , scale = 2, nullable = false)
 	private BigDecimal preco;
+	
+	//@NotNull(message = "O campo quantidade de convidados é obrigatório")
+	//@Min(value = 0 , message="Informe um valor igual ou maior que zero para o campo quantidade de convidados")
+	//@Max(value = 9999, message = "Informe um valor menor que 10 mil para o campo quantidade de convidados")
 	@Column(name = "quantidade_convidados", nullable = false)
 	private Long quantidadeConvidados;
+	
+	//@NotEmpty(message = "O campo descrição é obrigatório")
+	//@Size(min= 5 , max = 80 , message = "Tamanho inválido para o campo descrição(5 - 80)")
 	@Column(name = "descricao" , length = 80 , nullable = false)
 	private String  descricao;
 	
+	//@NotEmpty
 	@Column
 	private String caminhoImagem;
+	
+	//@NotEmpty(message = "O campo nome da combo é obrigatório")
 	@Column
 	private String nomeCombo;
 	
